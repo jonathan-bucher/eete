7c01772b585d59543343ff4dd7b45124a9a582c6
jonathan-bucher <jonathanbucher2@gmail.com>
2025-04-04 09:59:14 -0700

    breaking the eete function into utility for functions for testing and readability. Adding an error message for non binary instrumental variable. Beginning tests for eete funciton.

--- R/eete.R
+++ R/eete.R
@@ -18,6 +18,60 @@ inverse_fun = function(f, lower, upper) {
 
 
 
+
+#' Estimate the Egalitarian Equivalent Treatment Effect (eete) with Instrumental Variables
+#'
+#' This function estimates the eete, which optional arguments to include an IV or bootstrapped SE.
+#' It applies an input function to data, conditioning on treatment (and instrument variables, if applicable).
+#'
+#' @param inputFunction A function that will be applied to the data.
+#' @param ... Additional arguments to be passed to `inputFunction`.
+#' @param y The name of the outcome variable column in `data`.
+#' @param d The name of the treatment variable column in `data`.
+#' @param z Argument to include the name of the instrument variable column in `data`. z must only take values 0 or 1
+#' @param data The data frame containing the variables.
+#' @param f_inv Inverse function of f
+#' @return A list containing the estimated egalitarian equivalent treatment effect and the estimated variance.
+#' @import dplyr
+#' @importFrom boot boot
+
+# instrumental egalitarian equivalent function
+iv_eete = function(f, ..., y, d, z, data, indices, f_inv){
+  # create the option of using a bootstrap sample
+  bdata = data[indices,]
+  # check if z is binary
+  if (!all(bdata[[z]] %in% c(0, 1))) {
+    stop("instrumental variable z must only take values 0 or 1")
+  }
+  # filter data based on value of instrument
+  data1 = bdata %>%
+    dplyr::filter(!!sym(z) == 1)
+  data0 = bdata %>%
+    dplyr::filter(!!sym(z) == 0)
+  # extracting outcome variables for both groups
+  yz1 = data1[[y]]
+  yz0 = data0[[y]]
+  # compute proportion of treated individuals (d) in both groups
+  p1 = mean(data1[[d]], na.rm = TRUE)
+  p0 = mean(data0[[d]], na.rm = TRUE)
+  # compute mean transformed outcome (f applied to y) for both groups
+  fee_p1 = mean(f(yz1, ...), na.rm = TRUE)
+  fee_p0 = mean(f(yz0, ...), na.rm = TRUE)
+  # compute counterfactual outcome transformation for untreated and treated groups
+  fee1 = (((1 - p0) * fee_p1 - (1 - p1) * fee_p0) / (p1 - p0))
+  fee0 = ((p1 * fee_p0 - p0 * fee_p1) / (p1 - p0))
+  # compute difference in the inverse transformations of both counterfactuals
+  eete = (f_inv(fee1) - f_inv(fee0))
+
+  #INSERT VARIANCE HERE
+
+  var_eete = NULL
+
+  return(c(eete, var_eete))
+}
+
+
+
 #' Estimate the Egalitarian Equivalent Treatment Effect (eete)
 #'
 #' This function estimates the eete, which optional arguments to include an IV or bootstrapped SE.
@@ -27,7 +81,7 @@ inverse_fun = function(f, lower, upper) {
 #' @param ... Additional arguments to be passed to `inputFunction`.
 #' @param y The name of the outcome variable column in `data`.
 #' @param d The name of the treatment variable column in `data`.
-#' @param z Optional argument to include the name of the instrument variable column in `data`.
+#' @param z Optional argument to include the name of the instrument variable column in `data`. z must only take values 0 or 1
 #' @param data The data frame containing the variables.
 #' @param lower The lower bound for finding the inverse function value.
 #' @param upper The upper bound for finding the inverse function value.
@@ -45,60 +99,57 @@ inverse_fun = function(f, lower, upper) {
 
 eete = function(f, ..., y, d, z = NULL, data, lower = 0.1, upper = 100, se = NULL, B = 1000, f_inv = NULL) {
 
-
-
+  # store the target column of the data at the data_test variable
   data_test = data %>%
     dplyr::select(!!sym(y))
-
+  # handle a non numeric value returned by f
   if (is.character(f(data_test, ...))) {
     eete = f(data_test, ...)
-
-
-  } else {
-
-
+  }
+  else {
+    # checking if the function specified is one of the built in eete functions
     if (any(identical(f, eete::crpie), identical(f, eete::cdpie), identical(f, eete::cpiee))) {
-
+      # if f_inv is specified, change to null so this can be handled by the package
       if (!is.null(f_inv)) {
         f_inv = NULL
       }
+      # message for when lower and upper bounds are specified for built in functions
       if (lower != 0.1 | upper != 100){
-        message("lower and upper parameters are options for custom functions, which require bootstrapped standard errors and uniroot inverses. These parameters will not affect eete::crpie, eete::cdpie, or eete::cpiee functions.")
+        message("lower and upper parameters are options for custom functions, which require bootstrapped standard errors and uniroot inverses.
+                These parameters will not affect eete::crpie, eete::cdpie, or eete::cpiee functions.")
       }
-}
+    }
     if (is.null(f_inv)) {
-
+      # assign the inverse and inverse prime functions
       if (identical(f, eete::crpie)) {
         f_inv = function(x) eete::crpie_inv(x, ...)
         f_inv_prime = function(x) eete::crpie_inv_prime(x, ...)
-
-      } else if (identical(f, eete::cdpie)) {
+      }
+      else if (identical(f, eete::cdpie)) {
         f_inv = function(x) eete::cdpie_inv(x, ...)
         f_inv_prime = function(x) eete::cdpie_inv_prime(x, ...)
-
-      } else if (identical(f, eete::cpiee)) {
+      }
+      else if (identical(f, eete::cpiee)) {
         f_inv = function(x) eete::cpiee_inv(x, ...)
-
-        # INSERT f_inv_prime function here
-
+        f_inv_prime = function(x) eete::cpiee_inv_prime(x, ...)
+        # no closed for standard error for cpiee
         if (!is.null(se)){
           se = "boot"
           message("No theoretical SE for cpiee, using bootstrapped SE")
         }
-
-        f_inv_prime = function(x) x
-
-      } else {
+      }
+      else {
         f_inv = inverse_fun(function(x) f(x, ...), lower, upper)
+        # potential for this to be changed to generalized inverse prime function
         f_inv_prime = function(x) x
-
+        # no closed form standard error for custom functions
         if (!is.null(se)){
           se = "boot"
           message("No theoretical SE for custom function, using bootstrapped SE")
         }
       }
     }
-
+  # case where instrumental variables are specified
   if (!is.null(z)) {
 
   ee = function(data, indices){
@@ -132,7 +183,9 @@ eete = function(f, ..., y, d, z = NULL, data, lower = 0.1, upper = 100, se = NUL
 
 
 
-  } else {
+  }
+    # case where instrumental variables are not specified
+    else {
 
     ee = function(data, indices){
 
@@ -169,7 +222,8 @@ eete = function(f, ..., y, d, z = NULL, data, lower = 0.1, upper = 100, se = NUL
 
     eete = list(estimate = eete[1])
 
-  } else if (se == "theoretical") {
+  }
+  else if (se == "theoretical") {
 
 
     if (is.null(eete[2])){
@@ -178,12 +232,14 @@ eete = function(f, ..., y, d, z = NULL, data, lower = 0.1, upper = 100, se = NUL
       message("No theoretical SE function exists. Using bootstrapped SE.")
 
       eete = list(estimate = eete[1], se = se)
-    } else {
+    }
+    else {
       se = sqrt(eete[2])
       eete = list(estimate = eete[1], se = se)
     }
 
-  } else if (se == "boot") {
+  }
+  else if (se == "boot") {
     boot_results = boot::boot(data, ee, R = B)
     se = sd(boot_results$t[,1])
     eete = list(estimate = eete[1], se = se)
--- tests/test_eete.R
+++ tests/test_eete.R
@@ -1,6 +1,8 @@
 # testing the eete script 
 library(testthat)
+library(dplyr)
 source("C:/Users/jonat/OneDrive/economics_research/eete/R/eete.R")
+source("C:/Users/jonat/OneDrive/economics_research/eete/R/crpie.R")
 
 # testing the inverse function
 test_that("inverse_fun correctly computes inverse values", {
@@ -22,3 +24,30 @@ test_that("inverse_fun errors when no solution exists", {
 test_that("inverse_fun errors when given an invalid interval", {
   expect_error(inverse_fun(function(x) x^2, -1, -0.5)(4))
 })
+
+# testing eete and support functions
+
+# Create a small synthetic dataset
+set.seed(123)
+
+sample_data <- tibble(
+  mpg = rnorm(10, mean = 25, sd = 5),  # Outcome variable (fuel efficiency)
+  vs = sample(0:1, 10, replace = TRUE),  # Treatment indicator (engine type)
+  am = sample(0:1, 10, replace = TRUE)   # Instrumental variable (transmission type)
+)
+
+# testing the iv_eete function
+test_that("iv_eete handles non binary instrument", {
+  sample_data1 = sample_data
+  sample_data1[ ,"am"] = 2
+  expect_error(iv_eete(f = crpie, gam = 0.5, y = "mpg", d = "vs", z = "am",
+                       data = sample_data1, indices = 1:nrow(sample_data), f_inv = crpie_inv), 
+               "instrumental variable z must only take values 0 or 1")
+})
+
+
+
+
+
+
+
